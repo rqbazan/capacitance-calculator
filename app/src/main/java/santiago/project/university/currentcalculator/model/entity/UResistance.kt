b87761a36fbae2d37a3741d1e9607c8b7f60c6ed
RaqB <saquirozbazan@gmail.com>
2017-06-01 12:54:05 -0500

    added kiloohms

--- app/src/main/java/santiago/project/university/currentcalculator/model/entity/UResistance.kt
+++ app/src/main/java/santiago/project/university/currentcalculator/model/entity/UResistance.kt
@@ -12,5 +12,6 @@ import javax.measure.unit.Unit
 enum class UResistance(@StringRes val symbol: Int, @StringRes val fullname: Int, val unit: Unit<ElectricResistance>) {
     OHMIOS(R.string.symbol_ohmios, R.string.unit_ohmios, SI.OHM),
     MILIOHMIOS(R.string.symbol_miliohmios, R.string.unit_miliohmios, SI.MILLI(SI.OHM)),
-    NANOOHMIOS(R.string.symbol_nanoohmios, R.string.unit_nanoohmios, SI.NANO(SI.OHM))
+    NANOOHMIOS(R.string.symbol_nanoohmios, R.string.unit_nanoohmios, SI.NANO(SI.OHM)),
+    KILOOHMIOS(R.string.symbol_kiloohmios, R.string.unit_kiloohmios, SI.KILO(SI.OHM))
 }
